84c4d47905a29ac254b98d1b3258d5963d61e257
Jeroen Verhoeven <verhoevemans@gmail.com>
2024-10-27 22:09:16 +0100

    Issue #5 - An SVG image representing the Radar is visible

--- client/src/radar/views/details/radar-map/radar-map.component.ts
+++ client/src/radar/views/details/radar-map/radar-map.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Blip } from '../../../shared/models/blip.model';
+import { Radar } from '../../../shared/models/radar.model';
 
 @Component({
   selector: 'radar-map',
@@ -7,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './radar-map.component.scss'
 })
 export class RadarMapComponent {
+  @Input({ required: true })
+  public radar: Radar | undefined;
 
+  public getPosition(blip: Blip): string {
+    switch (this.radar?.quadrants.indexOf(blip.quadrant)) {
+      case 0:
+        return `translate(${-50}, ${-50})`;
+      case 1:
+        return `translate(${50}, ${-50})`;
+      case 2:
+        return `translate(${-50}, ${50})`;
+      case 3:
+      default:
+        return `translate(${50}, ${50})`;
+    }
+  }
 }
