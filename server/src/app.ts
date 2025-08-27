c0797255a0804a41625175b402279c765d2e63e6
Jeroen Verhoeven <verhoevemans@gmail.com>
2025-08-27 13:26:06 +0200

    Update CORS setting to allow GET, POST and PUT methods. Removed redundant proxy.conf file

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,6 +22,7 @@ async function createApp() {
     app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         next();
     });
 
