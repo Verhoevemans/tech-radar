09119e80a4b649b07f1411440e9ab510ebb9c56c
Jeroen Verhoeven <verhoevemans@gmail.com>
2025-08-30 14:48:12 +0200

    Update client url

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,7 +21,6 @@ async function createApp() {
 
     app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-        res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL_RAILWAY);
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         next();
