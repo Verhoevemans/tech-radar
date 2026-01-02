f6fb64552e550de3af3847f1f34d70d08431e0ff
Jeroen Verhoeven <verhoevemans@gmail.com>
2026-01-02 21:40:12 +0100

    Issue #17 - Update to Angular 21

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { radarConfig } from './radar/radar.config';
 import { RadarComponent } from './radar/radar.component';
 
-bootstrapApplication(RadarComponent, radarConfig)
+bootstrapApplication(RadarComponent, {...radarConfig, providers: [provideZoneChangeDetection(), ...radarConfig.providers]})
   .catch((err) => console.error(err));
